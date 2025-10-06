d09f75698fdd4f5d4ef3a2062ca9fe567dccd288
huyngvan395 <ngvanhuy0000@gmail.com>
2025-10-06 16:11:52 +0700

    new

--- Chat_Server/src/main/java/org/example/chatserver/Utilities/CloudinaryConfig.java
+++ Chat_Server/src/main/java/org/example/chatserver/Utilities/CloudinaryConfig.java
@@ -2,12 +2,16 @@ package org.example.chatserver.Utilities;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class CloudinaryConfig {
+    private static Dotenv dotenv = Dotenv.load();
+    private static String api_key_cloudinary = dotenv.get("API_KEY_CLOUDINARY");
+    private static String api_key_secret = dotenv.get("API_KEY_SECRET");
     private static final Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
             "cloud_name", "digs0j48l",
-            "api_key", "974685814667245",
-            "api_secret", "8LZlDdXJ9LFCgW42T9IJ2-4bh2c"));
+            "api_key", api_key_cloudinary,
+            "api_secret", api_key_secret));
 
     public static Cloudinary getCloudinary() {
         return cloudinary;
